ebbab824580fdf08490b1fcb1bfcae72ea772779
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-11-10 13:43:13 -0700

    Updating Java, SpringBoot and adding github support

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM openjdk:11-jre
-RUN useradd -ms /bin/bash app
-USER app
+FROM amazoncorretto:21-al2023-headless
+USER nobody
 WORKDIR /home/app
 COPY target/*.jar /home/app/
 HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD curl --fail http://localhost:8080/actuator/info || exit 1
--- src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.truemark.demo.helloworld;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
 @RestController
