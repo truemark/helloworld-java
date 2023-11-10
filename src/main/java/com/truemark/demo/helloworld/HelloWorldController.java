c515ad76f47422358b21ffd769953bf936133edb
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-11-10 12:36:40 -0700

    Adding some additional debug

--- src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 @Slf4j
@@ -23,10 +24,37 @@ public class HelloWorldController {
         private String message;
     }
 
+    private void logRequest(HttpServletRequest req) {
+        String str = "\nIngress Request:\n" +
+                "  URI:       %s\n" +
+                "  Scheme:    %s\n" +
+                "  Protocol:  %s\n" +
+                "  Server:    %s:%d\n" +
+                "  Headers:\n%s\n";
+        StringBuilder headers = new StringBuilder();
+        Enumeration<String> names = req.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            Enumeration<String> values = req.getHeaders(name);
+            while (values.hasMoreElements()) {
+                String value = values.nextElement();
+                headers.append("    ").append(name).append(": ").append(value).append("\n");
+            }
+        }
+        str = String.format(str,
+                req.getRequestURI(),
+                req.getScheme(),
+                req.getProtocol(),
+                req.getServerName(),
+                req.getServerPort(),
+                headers);
+        log.trace(str);
+    }
+
     @RequestMapping(value = "**")
     public Hello hello(HttpServletRequest req) {
         if (log.isTraceEnabled()) {
-            log.trace(String.format("Received request from %s to %s",req.getRemoteAddr(), req.getRequestURI()));
+            logRequest(req);
         }
         return new Hello().setMessage(message);
     }
