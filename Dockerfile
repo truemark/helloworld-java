9710f285664392f5e77be5f32649aa32f6fd43d1
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-11-10 12:36:40 -0700

    Added logging, message override and upgraded Spring

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN useradd -ms /bin/bash app
 USER app
 WORKDIR /home/app
 COPY target/*.jar /home/app/
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD curl --fail http://localhost:8080/health || exit 1
 EXPOSE 8080
-ENTRYPOINT ["/bin/bash", "-c", "java -jar *.jar"]
+ENTRYPOINT ["/bin/bash", "-c", "java -Xmx64m -jar *.jar"]
--- src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/truemark/demo/helloworld/HelloWorldController.java
@@ -2,21 +2,33 @@ package com.truemark.demo.helloworld;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Slf4j
 public class HelloWorldController {
+
+    // This value comes from application.yml or if set an environment variables called MESSAGE
+    @Value("${message}")
+    private String message;
 
     @Data
     @Accessors(chain = true)
-    public class Hello {
+    public static class Hello {
         private String message;
     }
 
     @RequestMapping(value = "**")
-    public Hello hello() {
-        return new Hello().setMessage("Hello world!");
+    public Hello hello(HttpServletRequest req) {
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("Received request from %s to %s",req.getRemoteAddr(), req.getRequestURI()));
+        }
+        return new Hello().setMessage(message);
     }
 
 }
