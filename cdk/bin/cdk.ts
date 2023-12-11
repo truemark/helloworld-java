25d59eec08e88715781f7355b1c7873bbba9c240
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-12-11 12:32:27 -0700

    Adding CDK stack

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import {DataClassification, ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
+import {AppStack} from '../lib/app-stack';
+import {StringHelper} from 'truemark-cdk-lib/helpers';
+
+const app = new ExtendedApp({
+  standardTags: {
+    automationTags: {
+      id: "helloworld-java",
+      url: "https://github.com/truemark/helloworld-java.git"
+    },
+    costCenterTags: {
+      businessUnitName: "technology",
+      projectName: "helloworld-java"
+    },
+    securityTags: {
+      dataClassification: DataClassification.Public
+    },
+    teamTags: {
+      name: "technology"
+    }
+  }
+});
+
+let env = app.node.tryGetContext("env");
+if (!env) {
+  throw new Error("Missing env in context");
+}
+env = StringHelper.toPascalCase(env);
+
+new AppStack(app, `HelloWorld${env}`, {
+  env: { account: app.account, region: app.region}
+});
+
